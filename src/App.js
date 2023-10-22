e003e4ed71c675e4f9518813c21a89202831bc10
khameer_13 <khameer2002@gmail.com>
2023-10-22 10:26:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import { createElement } from 'react';
 import './App.css';
+import MyComponent from './todoElement';
+
+
+function addnote() {
+  const inputhead = document.getElementById("inputhead").value;
+  const inputbody = document.getElementById("inputbody").value;
+  if(inputhead!=""){
+  
+    let newcomp = document.createElement("div");
+    newcomp.innerHTML =
+      `<h2>${inputhead}</h2>
+    <p>${inputbody}</p>`;
+    newcomp.classList.add('listelement');
+    console.log("khameer");
+    // newcomp.style.className='listelement';    incorrect way to add class
+    newcomp.classList.add('listelement');   //correct way to add class
+    const noteContainer=document.getElementById("noteContainer");
+    noteContainer.appendChild(newcomp);
+    document.getElementById("inputhead").value = "";
+    document.getElementById("inputbody").value=""
+
+  }
+
+  else{
+    alert("please provide a file name")
+  }
+
+
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>To-Do list</h1>
+      <div className='box'>
+        <div id="noteContainer" className='todobox' >
+       
+
+        </div>
+        <div className='inputbox'>
+          <input id="inputhead" type="text" placeholder="name" />
+          <textarea id="inputbody" type="text" placeholder="Enter your text" />
+          <button type="button" onClick={addnote} >  Add Note </button>
+        </div>
+
+      </div>
+    </>
   );
 }
 
---
+++ src/todoElement.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+
+        <div className='listelement'>   
+        <h2 id="head">heading</h2> 
+        <p id='data' >paragraph</p>
+         </div>
+
+    );
+  }
+}
+
+export default MyComponent;
+
